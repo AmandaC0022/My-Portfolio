d23999977d8c73a881f922fea6901b85c4f55051
Amanda Morgan <AmandaC0022@gmail.com>
2023-07-09 15:08:45 -0500

    added homepage

--- my-react-portfolio/src/pages/home.js
+++ my-react-portfolio/src/pages/home.js
@@ -1,8 +1,15 @@
-
+import image from '../images/IMG_3563.png'
 const Homepage = () => {
     return ( 
-        <div className="body">
-            <p>This is my portfolio.</p>
+        <div className="home-container">
+            <img href={image} alt="my portrait"/> 
+            <div className="home-text-container">
+                <h1>Amanda Morgan</h1>
+                <hr/>
+                <h3>Artist</h3>
+                <h3>Designer</h3>
+                <h3>Developer</h3>
+            </div>
         </div>
      );
 }
