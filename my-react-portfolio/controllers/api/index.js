1cca7b3eb71ff1aaf9d21ade3a3981d14a38f89c
Amanda Morgan <AmandaC0022@gmail.com>
2024-05-28 16:43:34 -0500

    backend server running successfully

--- my-react-portfolio/controllers/api/index.js
+++ my-react-portfolio/controllers/api/index.js
@@ -1,16 +1,8 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const nicheRoutes = require("./nicheRoutes");
-
-const collectionRoutes = require("./collectionRoutes");
 const hashtagRoutes = require("./hashtagRoutes");
-const hashtagnicheRoutes = require("./hashtagnicheRoutes");
 const collectiontagRoutes = require("./collectiontagRoutes");
 
-router.use('/users', userRoutes);
-router.use('/niches', nicheRoutes);
 router.use("/hashtags", hashtagRoutes);
-router.use("/user_hashtag", hashtagnicheRoutes); 
 router.use("/collectiontags", collectiontagRoutes);
 
 module.exports = router;
