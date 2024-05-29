f174da6e4713535ebf721d0c3cfd0780ac37f3f0
Amanda Morgan <AmandaC0022@gmail.com>
2024-05-29 10:52:53 -0500

    added new projects to project list page

--- my-react-portfolio/src/pages/Homepage.js
+++ my-react-portfolio/src/pages/Homepage.js
@@ -11,6 +11,7 @@ const Homepage = () => {
                     <h3>Designer</h3>
                     <h3>Ecommerce</h3>
                 </div>
+                <p>I am a dedicated Web Developer and Designer who is committed to building intuitive, user-friendly digital experiences. I describe myself as an innovative problem solver, passionate about web design and development, with a go-getter attitude and high attention to detail, that is not afraid to take on tasks outside of my job description. With each project, my goal is to always be learning and tackle new opportunities with a positive attitude. I am excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web. </p>
             </div>
         </div>
      );
--- my-react-portfolio/src/pages/Projects.js
+++ my-react-portfolio/src/pages/Projects.js
@@ -1,95 +1,28 @@
+import bluestockimage from "../images/Bluestock.jpg"; 
+import cwimage from '../images/IMG_0040.jpg'; 
+import cffimage from '../images/IMG_0041.jpg';
+import kukaimage from '../images/Kuka-Home-Furnishings.jpg'; 
 import scimage from '../images/IMG_7369.jpg'; 
 import gabbyimage from '../images/IMG_7365.jpg';
 import gabbyfabrics from '../images/IMG_7367.jpg'; 
 import scfabrics from '../images/IMG_7368.jpg';
 import contractimage from '../images/IMG_7366.jpg';   
 import scblog from '../images/IMG_7369.jpg'; 
-import gabbyblog from '../images/IMG_7370.jpg'; 
+import gabbyblog from '../images/IMG_7370.jpg';
+import ProjectList from '../projects/ProjectList.json';  
 // import gergsworld from '../images/'; 
 
 const Projects = () => {
-    const projectList = [
-        {
-            image:`${scimage}`, 
-            title:"checkworks.com",
-            url:"https://checkworks.com/", 
-            role:"Frontend Developer and UX Designer", 
-            framework:"Custom built with React and TypeScript frontend and C#, .NET backend and MySQL database", 
-            description:"As apart of the Ecommerce team, I designed and built responsive full stack code  ", 
-        }, 
-        {
-            image:`${scimage}`, 
-            title:"summerclassics.com",
-            url:"https://summerclassics.com/", 
-            role:"Project Manager, UX Designer, Power User and Business Analyst", 
-            framework:"Magento", 
-            description:"I was a part of a small internal team that project managers, designed, developed and launched this Magento Commerce site. Post launch, I continue to manage, design and develop new user interfaces within the magento framework.", 
-        }, 
-        {
-            image:`${gabbyimage}`, 
-            title:"gabby.com", 
-            url:"https://gabby.com/",
-            role:"Project Manager, UX Designer, Power User and Business Analyst", 
-            framework:"Magento",
-            description:"This site is the sister brand to Summer Classics. This site was launched accompanying the launch of summerclassics.com. Post launch, I continue to design, develop, and manage this Magneto Commerce site.", 
-        },
-        {
-            image:`${gabbyfabrics}`, 
-            title:"fabrics.gabby.com", 
-            url:"https://fabrics.gabby.com/",
-            role:"Project Manager, UX Designer, Power User and Business Analyst", 
-            framework:"Wordpress",
-            description:"This site handles free fabric samples orders. I worked with an outside agency to develop and design this Wordpress WooComerce site. This site launched a week prior to the launch of the E-commerce sites. Post launch, I am managing the product data.", 
-        }, 
-        {
-            image:`${scfabrics}`, 
-            title:"fabrics.summerclassics.com", 
-            url:"https://fabrics.summerclassics.com/",
-            role:"Project Manager, UX Designer, Power User and Business Analyst", 
-            framework:"Wordpress",
-            description:"This site handles free fabric samples orders for summer classics outdoor performance fabrics. I worked with an outside agency to develop and design this Wordpress WooComerce site. This site launched a week prior to the launch of the E-commerce sites. Post launch, I am managing the product data.", 
-        },
-        {
-            image:`${contractimage}`, 
-            title:"contract.summerclassics.com", 
-            url:"https://contract.summerclassics.com/",
-            role:"Power User and Business Analyst", 
-            framework:"Magento",
-            description:"I handle the daily oppations for this site. I worked with a software developer to generate product feeds from the ERP system. This site is now entirely feed and populated by those feeds. Now, I just handle content updates.", 
-        },
-        {
-            image:`${scblog}`, 
-            title: "summerclassics.com/blog", 
-            url:"https://summerclassics.com/blog/",
-            role:"Project Manager, UX Designer and Power User", 
-            framework:"Wordpress",
-            description:"I was head of a small team to build this content site. I worked very closely with an outside agency to design and develop this site. This site was launch in random with the new Commerce sites. Post launch, I manage this site and handle all content updates.", 
-        },
-        {
-            image:`${gabbyblog}`, 
-            title: "gabby.com/blog/", 
-            url:"https://gabby.com/blog",
-            role:"Project Manager, UX Designer and Power User", 
-            framework:"Wordpress",
-            description:"This is the sister site to blog.summerclassics.com. Post launch, I now manage this site and handle all content updates.", 
-        },
-        // {
-        //     image:{gergsworld}, 
-        //     url: "", 
-        //     role:"UX Designer and Developer", 
-        //     framework:"React, MongoDB, Express, and GraphQL",
-        //     description:"This is a fun website that I am making for my father. I am using the traditional MERN stack and building this site from scratch.", 
-        // },
-    ]
+    console.log(ProjectList); 
     return ( 
         <div>
             <h2>Projects</h2>
             <hr/>
             <div className="project-container">
-                {projectList.map((project) => (
+                {ProjectList.projects.map((project) => (
                     <div className="project">
                         <div className="container">
-                            <img src={project.image} alt={project.url}/>
+                            <img src={project.imagepath} alt={project.url}/>
                             <div className="overlay">
                                 <button className="custombtn"><a href={project.url} target="_blank" rel="noreferrer">Visit Site</a></button>
                             </div>
@@ -97,6 +30,7 @@ const Projects = () => {
                         <h4>{project.title}</h4>
                         <button className="custombtn mobilebtn"><a href={project.url} target="_blank" rel="noreferrer">Visit Site</a></button>
                         <p>Role: {project.role}</p>
+                        <p>Company: {project.company}</p>
                         <p>Framework: {project.framework}</p>
                         <p>{project.description}</p>
                     </div>
