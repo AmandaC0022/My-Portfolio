c5181b1e1c137bb17a1f60f32bd9fedff86547c5
Amanda Morgan <AmandaC0022@gmail.com>
2024-06-04 15:30:29 -0500

    working on creating a react component for Project to display within Projects

--- my-react-portfolio/src/pages/Projects/Projects.js
+++ my-react-portfolio/src/pages/Projects/Projects.js
@@ -1,40 +1,34 @@
 
-import './Projects.css'; 
-import bluestockimage from "../../images/Bluestock.jpg"; 
-import cwimage from '../../images/IMG_0040.jpg'; 
-import cffimage from '../../images/IMG_0041.jpg';
-import kukaimage from '../../images/Kuka-Home-Furnishings.jpg'; 
-import scimage from '../../images/IMG_7369.jpg'; 
-import gabbyimage from '../../images/IMG_7365.jpg';
-import gabbyfabrics from '../../images/IMG_7367.jpg'; 
-import scfabrics from '../../images/IMG_7368.jpg';
-import contractimage from '../../images/IMG_7366.jpg';   
-import scblog from '../../images/IMG_7369.jpg'; 
-import gabbyblog from '../../images/IMG_7370.jpg';
-import AllProjectList from '../../projectLists/AllProjectList.json';  
-// import gergsworld from '../images/'; 
+import './Projects.css';
+import AllProjectList from '../../projectLists/AllProjectList.json'; 
+import Project from '../../components/Project/Project';  
 
 const Projects = () => {
-    const returnedProjectList = []; 
+    let returnedProjectList = []; 
+    let givenValue = ""; 
+
+    const getValue = (event) => {
+        givenValue = event.target.value.toString(); 
+        console.log(givenValue); 
+        returnProjects(); 
+    }
 
     //sorts through and only returns projects that match the given framework and stores the results in the returnedProjectList array
     const returnProjects = () => {
         AllProjectList.projects.find((project) => {
-            if ((project.framework).includes("Wordpress")) {
-                returnedProjectList.push(project); 
+            if ((project.framework).includes(givenValue)) {
+                returnedProjectList.push(project);  
             }
         })
     }; 
-    
-    return ( 
-        <div>
-            <h2>Projects</h2>
-            <div className="project-container fadeIn">
-                {returnProjects()}
-                {returnedProjectList.map((project) => (
+
+    const displayAllProjects = () => {
+        return (
+            AllProjectList.projects.map((project) => {
+                return(
                     <div className="project">
                         <div className="container">
-                            <img src={bluestockimage} alt={project.url}/>
+                            <iframe width="100%" src={project.url} title={project.title} scrolling="no"></iframe>
                             <div className="overlay">
                                 <button className="custombtn"><a href={project.url} target="_blank" rel="noreferrer">Visit Site</a></button>
                             </div>
@@ -46,7 +40,48 @@ const Projects = () => {
                         <p>Framework: {project.framework}</p>
                         <p>{project.description}</p>
                     </div>
-                ))}
+                )
+            })
+        )
+    }
+    const displayCertainProjects = () => {
+        return (
+            returnedProjectList.map((project) => {
+                <div className="project">
+                    <div className="container">
+                        <iframe width="100%" src={project.url} title={project.title} scrolling="no"></iframe>
+                        <div className="overlay">
+                            <button className="custombtn"><a href={project.url} target="_blank" rel="noreferrer">Visit Site</a></button>
+                        </div>
+                    </div>
+                    <h4>{project.title}</h4>
+                    <button className="custombtn mobilebtn"><a href={project.url} target="_blank" rel="noreferrer">Visit Site</a></button>
+                    <p>Role: {project.role}</p>
+                    <p>Company: {project.company}</p>
+                    <p>Framework: {project.framework}</p>
+                    <p>{project.description}</p>
+                </div>
+            })
+        )
+        } 
+        
+    
+    return ( 
+        <div>
+            <div className="project-header">
+                <h2>Projects</h2>
+                <p>I love working with a team in a fast paced environment. I tend to take charge, always keep busy and maintain a positive attitude. </p>
+                <div className="project-button-container">
+                    <button className="custom-btn" value="Custom" onClick={getValue}>See Custom Projects</button>
+                    <button className="custom-btn" value="Wordpress" onClick={getValue}>See Wordpress Projects</button>
+                    <button className="custom-btn" value="Magento" onClick={getValue}>See Magento Projects</button>
+                </div>
+            </div>
+            <div className="project-container fadeIn">
+                {/* {displayAllProjects()} */}
+                <Project/>
+                <Project/>
+                <Project/>
             </div>
         </div>
      );
