3ae1dce3f97d84eb1a8a1e4f395101cfc168149a
Amanda Morgan <AmandaC0022@gmail.com>
2024-06-04 16:15:24 -0500

    styled resume page

--- my-react-portfolio/src/pages/Resume/Resume.js
+++ my-react-portfolio/src/pages/Resume/Resume.js
@@ -1,10 +1,14 @@
 import pdf from '../../images/2024WebDevelopmentResume.pdf'; 
+import './Resume.css'; 
+import Circles from '../../components/Circles/Circles'
 
 const Resume = () => {
     return ( 
         <div className="resume-container">
-            <h2>Resume</h2>
-            <hr/>
+            <div className="resume-header">
+                <h2>Resume</h2>
+            </div>
+            <Circles/>
             <object data={pdf} type="application/pdf" width="100%" height="700px" aria-labelledby="2024 Web Development Resume" alt="2024 Web Development Resume"></object>
         </div>
      );
