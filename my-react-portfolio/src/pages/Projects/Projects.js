d9d6c638482e9590676c341094c5cc565c984e5c
Amanda Morgan <AmandaC0022@gmail.com>
2024-06-14 17:22:34 -0500

    working on project filter functionality

--- my-react-portfolio/src/pages/Projects/Projects.js
+++ my-react-portfolio/src/pages/Projects/Projects.js
@@ -3,35 +3,36 @@ import './Projects.css';
 import AllProjectList from '../../projectLists/AllProjectList.json'; 
 import Project from '../../components/Project/Project';  
 import Circles from '../../components/Circles/Circles'; 
+import { useState, useEffect } from 'react';
 
 const Projects = () => {
-    let returnedProjectList = []; 
+    const [projectList, setProjectList] = useState(AllProjectList.projects);  
     let givenValue = ""; 
 
-    const getValue = (event) => {
+    const getValue = (event) => { 
         givenValue = event.target.value.toString(); 
-        console.log(givenValue); 
-        returnProjects(); 
-    }
+        filterProjects(givenValue); 
+    };  
 
-    //sorts through and only returns projects that match the given framework and stores the results in the returnedProjectList array
-    const returnProjects = () => {
-        AllProjectList.projects.find((project) => {
-            if ((project.framework).includes(givenValue)) {
-                returnedProjectList.push(project);  
-            }
-        })
+    const filterProjects = (value) => {
+        setProjectList(projectList.filter(project => project.framework === value));
     }; 
 
-    const displayAllProjects = () => {
+    const resetProjects = () => {
+        setProjectList(AllProjectList.projects); 
+    }; 
+
+    const displayProjects = () => {
+        console.log(projectList); 
         return (
-            AllProjectList.projects.map((project) => { 
-                return(
+            projectList.map((project) => {
+                return ( 
                     <Project 
+                        key={project.title}
                         title={project.title} 
                         description={project.description} 
                         url={project.url}
-                        image={project.imagepath}
+                        imagepath={project.imagepath}
                         role={project.role}
                         company={project.company}
                         framework={project.framework}
@@ -41,31 +42,23 @@ const Projects = () => {
                 )
             })
         )
-    }; 
+    }
 
-    const displayCertainProjects = () => {
-        return (
-            returnedProjectList.map((project) => {
-                <Project title={project.title} description={project.description} url={project.url}/>
-            })
-        )
-    };  
-        
-    
     return ( 
         <div className="project-container fadeIn">
             <div className="project-header">
                 <h2>Projects</h2>
                 <p>I love working with a team in a fast paced environment. I tend to take charge, always keep busy and maintain a positive attitude. </p>
-                {/* <div className="project-button-container">
+                <div className="project-button-container">
                     <button className="custom-btn fill" value="Custom" onClick={getValue}><p>See Custom Projects</p></button>
                     <button className="custom-btn fill" value="Wordpress" onClick={getValue}><p>See Wordpress Projects</p></button>
                     <button className="custom-btn fill" value="Magento" onClick={getValue}><p>See Magento Projects</p></button>
-                </div> */}
+                    <button className="custom-btn fill" onClick={resetProjects}><p>See All Projects</p></button>
+                </div>
             </div>
             <Circles/>
             <div className="project-list-container">
-                {displayAllProjects()}
+                {displayProjects()}
             </div>
         </div>
      );
